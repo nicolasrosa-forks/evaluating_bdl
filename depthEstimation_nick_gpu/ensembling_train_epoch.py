af2e723f2543f6fe4e06f212e62667614348c6fc
nicolasrosa <nicolas.rosa@hotmail.com>
2021-11-23 15:45:03 -0300

    [Mod] Pytorch optimizations

--- depthEstimation_nick_gpu/ensembling_train_epoch.py
+++ depthEstimation_nick_gpu/ensembling_train_epoch.py
@@ -15,6 +15,7 @@ import os
 from datetime import datetime
 from glob import glob
 from send2trash import send2trash
+from tqdm import tqdm
 
 # Torch Libraries
 import torch
@@ -55,6 +56,9 @@ args = parser.parse_args()
 # ================== #
 debug = False
 model_id = "ensembling"
+
+num_gpus = torch.cuda.device_count()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # kitti_depth_path = "/root/data/kitti_depth"
 # kitti_rgb_path = "/root/data/kitti_rgb"
@@ -178,18 +182,19 @@ def main():
 
     # Load Train Dataset
     train_dataset = DatasetKITTIAugmentation(kitti_depth_path=kitti_depth_path, kitti_rgb_path=kitti_rgb_path, crop_size=(352, 352))
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=4*num_gpus)
     num_train_steps = len(train_loader)
 
     # Load Evaluation Dataset
     val_dataset = DatasetKITTIVal(kitti_depth_path=kitti_depth_path)
-    val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=val_batch_size, shuffle=False, num_workers=1)
+    val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=val_batch_size, shuffle=False, num_workers=4*num_gpus)
     num_val_samples = len(val_loader)
 
     # --- Models Loop --- #
-    for m in range(M):
+    for m in tqdm(range(M), desc="Models"):
         # Network Architecture
-        model = DepthEstimationNet(pretrained=True).cuda()
+        # model = DepthEstimationNet(pretrained=True).cuda()
+        model = DepthEstimationNet(pretrained=True).to(device)
 
         if args.restore is not None:
             model.load_state_dict(torch.load(args.restore))
@@ -208,8 +213,10 @@ def main():
         optimizer.zero_grad()
 
         # Evaluation criteria
-        criterion = MaskedL2Gauss().cuda()
-        rmse_criterion = RMSE().cuda()
+        # criterion = MaskedL2Gauss().cuda()
+        # rmse_criterion = RMSE().cuda()
+        criterion = MaskedL2Gauss().to(device)
+        rmse_criterion = RMSE().to(device)
 
         # Summary
         dt_string = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -225,14 +232,22 @@ def main():
 
         last_sum_val_batch_rmses = 1e9
 
-        # --- Training Loop --- #
-        for epoch in range(args.num_epochs):
-            for i_iter, batch in enumerate(train_loader):
-                imgs, _, targets, file_ids = batch
+        # --- Training Loop --- # 
+        for epoch in tqdm(range(args.num_epochs), desc="Epochs"):
+            model.train()
+            
+            running_loss = 0
+            running_loss_rmse = 0
+            tk = tqdm(train_loader, desc="Train")
 
+            for i_iter, batch in enumerate(tk, start=1):
+                imgs, _, targets, file_ids = batch
+                
                 # Input tensors (Placeholders)
-                imgs = Variable(imgs).cuda()              # (shape: (batch_size, h, w, 3))
-                targets = Variable(targets).cuda()        # (shape: (batch_size, h, w))
+                # imgs = Variable(imgs).cuda()              # (shape: (batch_size, h, w, 3))
+                # targets = Variable(targets).cuda()        # (shape: (batch_size, h, w))
+                imgs = imgs.to(device)              # (shape: (batch_size, h, w, 3))
+                targets = targets.to(device)        # (shape: (batch_size, h, w))
 
                 # Outputs tensors
                 means, log_vars = model(imgs)  # (both will have shape: (batch_size, 1, h, w))
@@ -252,12 +267,21 @@ def main():
                 # train_batch_losses.append(loss_cpu)
                 # train_batch_rmses.append(rmse_cpu)
                 
-                print("%d/%d, %d/%d, %d/%d, loss: %g, RMSE: %g" % (m+1, M, epoch+1, args.num_epochs, i_iter+1, num_train_steps, loss_cpu, rmse_cpu))
+                # print("%d/%d, %d/%d, %d/%d, loss: %g, RMSE: %g" % (m+1, M, epoch+1, args.num_epochs, i_iter+1, num_train_steps, loss_cpu, rmse_cpu))
+                
+                running_loss += loss.item()
+                running_loss_rmse += rmse.item()
+
+                running_loss_i = running_loss/i_iter
+                tk.set_postfix(loss=running_loss_i)
 
                 # Summary
-                writer.add_scalar('Train/Loss', loss_cpu, epoch*num_train_steps + i_iter)
-                writer.add_scalar('Train/RMSE', rmse_cpu, epoch*num_train_steps + i_iter)
-                writer.add_scalar('Train/LR', get_lr(optimizer), epoch*num_train_steps + i_iter)
+                step = epoch*num_train_steps + i_iter
+                # writer.add_scalar('Train/Loss', loss_cpu, step)
+                # writer.add_scalar('Train/RMSE', rmse_cpu, step)
+                writer.add_scalar('Train/Loss_v2', running_loss_i, step)
+                writer.add_scalar('Train/RMSE_v2', running_loss_rmse/i_iter, step)
+                writer.add_scalar('Train/LR', get_lr(optimizer), step)
                 writer.flush()  # Call flush() method to make sure that all pending events have been written to disk.
 
                 # Visualization
@@ -321,52 +345,67 @@ def main():
                     k = cv2.waitKey(25)
                     if k == 27:  # Esc key to stop
                         break
+            
+            # print('Training loss: {:.6f}'.format(running_loss/num_train_steps))
 
             # Free GPU memory
             del imgs, targets
-            torch.cuda.empty_cache()
+            torch.cuda.empty_cache() # FIXME: augusto me falou que não é recomendado utilizar essa função
 
-            # ----- Validation Loop ----- #
-            sum_val_batch_losses = 0
-            sum_val_batch_rmses = 0
-            for k_iter, val_batch in enumerate(val_loader):
-                imgs, _, targets, file_ids = val_batch
+            # ----- Validation Loop ----- # FIXME: Implementar torch.no_grad()
+            with torch.no_grad():
+                model.eval()
 
-                # Input tensors (Placeholders)
-                imgs = Variable(imgs).cuda()        # (shape: (batch_size, h, w, 3))
-                targets = Variable(targets).cuda()  # (shape: (batch_size, h, w))
+                sum_val_batch_losses = 0
+                sum_val_batch_rmses = 0
+                val_tk = tqdm(val_loader, "Val")
 
-                # Outputs tensors
-                means, log_vars = model(imgs)  # (both will have shape: (batch_size, 1, h, w))
+                for k_iter, val_batch in enumerate(val_tk, start=1):
+                    imgs, _, targets, file_ids = val_batch
 
-                val_loss = criterion(means, log_vars, targets)
-                val_rmse = rmse_criterion(means, targets)
+                    # Input tensors (Placeholders)
+                    # imgs = Variable(imgs).cuda()        # (shape: (batch_size, h, w, 3))
+                    # targets = Variable(targets).cuda()  # (shape: (batch_size, h, w))
+                    imgs = imgs.to(device)        # (shape: (batch_size, h, w, 3))
+                    targets = targets.to(device)  # (shape: (batch_size, h, w))
 
-                val_loss_cpu = val_loss.data.cpu().numpy()
-                val_rmse_cpu = val_rmse.data.cpu().numpy()
+                    # Outputs tensors
+                    means, log_vars = model(imgs)  # (both will have shape: (batch_size, 1, h, w))
 
-                sum_val_batch_losses += val_loss_cpu
-                sum_val_batch_rmses += val_rmse_cpu
+                    val_loss = criterion(means, log_vars, targets)
+                    val_rmse = rmse_criterion(means, targets)
 
-                # print("%d/%d, %d/%d, %d/%d, loss: %g, RMSE: %g" % (m+1, M, epoch+1, args.num_epochs, k_iter+1, num_val_samples, val_loss_cpu, val_rmse_cpu))
-                print(f"{m+1}/{M}, {epoch+1}/{args.num_epochs}, {k_iter+1}/{num_val_samples}, loss: {val_loss_cpu}, RMSE: {val_rmse_cpu}")
+                    # val_loss_cpu = val_loss.data.cpu().numpy()
+                    # val_rmse_cpu = val_rmse.data.cpu().numpy()
+
+                    sum_val_batch_losses += val_loss.item()
+                    sum_val_batch_rmses += val_rmse.item()
+
+                    # print("%d/%d, %d/%d, %d/%d, loss: %g, RMSE: %g" % (m+1, M, epoch+1, args.num_epochs, k_iter+1, num_val_samples, val_loss_cpu, val_rmse_cpu))
+                    # print(f"{m+1}/{M}, {epoch+1}/{args.num_epochs}, {k_iter+1}/{num_val_samples}, loss: {val_loss_cpu}, RMSE: {val_rmse_cpu}")
+                    val_tk.set_postfix(val_loss=sum_val_batch_losses/(k_iter))
+
+                # print('Validation loss: {:.6f}'.format(val_loss/num_val_samples))
+
+                # Summary
+                # writer.add_scalar('Val/Loss', sum_val_batch_losses/num_val_samples, epoch)
+                # writer.add_scalar('Val/RMSE', sum_val_batch_rmses/num_val_samples, epoch)
+                writer.add_scalar('Val/Loss_v2', sum_val_batch_losses/num_val_samples, epoch)
+                writer.add_scalar('Val/RMSE_v2', sum_val_batch_rmses/num_val_samples, epoch)
                 
-            # Summary
-            writer.add_scalar('Val/Loss', sum_val_batch_losses/num_val_samples, epoch)
-            writer.add_scalar('Val/RMSE', sum_val_batch_rmses/num_val_samples, epoch)
-            writer.flush()  # Call flush() method to make sure that all pending events have been written to disk.
+                writer.flush()  # Call flush() method to make sure that all pending events have been written to disk.
 
-            if last_sum_val_batch_rmses > sum_val_batch_rmses:
-                print(f"The model improved from {last_sum_val_batch_rmses} to {sum_val_batch_rmses}")
-                # save the model weights to disk:
-                model_checkpoint_filepath = current_run_dir + "/model_M" + str(m) + "_epoch_" + str(epoch+1) + ".pth"
-                torch.save(model.state_dict(), model_checkpoint_filepath)
+                if last_sum_val_batch_rmses > sum_val_batch_rmses:
+                    print(f"The model improved from {last_sum_val_batch_rmses} to {sum_val_batch_rmses}")
+                    # save the model weights to disk:
+                    model_checkpoint_filepath = current_run_dir + "/model_M" + str(m) + "_epoch_" + str(epoch+1) + ".pth"
+                    torch.save(model.state_dict(), model_checkpoint_filepath)
 
-            last_sum_val_batch_rmses = sum_val_batch_rmses
+                last_sum_val_batch_rmses = sum_val_batch_rmses
 
-            # Free GPU memory
-            del imgs, targets
-            torch.cuda.empty_cache()
+                # Free GPU memory
+                del imgs, targets
+                torch.cuda.empty_cache() # FIXME: augusto me falou que não é recomendado utilizar essa função
 
         # Close SummaryWriter
         writer.close()
