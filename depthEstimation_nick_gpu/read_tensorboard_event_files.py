2973b0d018551de0c9f087e2ae4e6b2c22f2ce3c
nicolasrosa <nicolas.rosa@hotmail.com>
2021-12-10 14:10:45 -0300

    [Mod] aux scripts updated

--- depthEstimation_nick_gpu/read_tensorboard_event_files.py
+++ depthEstimation_nick_gpu/read_tensorboard_event_files.py
@@ -23,24 +23,34 @@ def main():
 
     print(f"Num of 'runs' found: {len(files)}")
 
+    delete_it = False
+
     for file in files:
         try:
             ea = event_accumulator.EventAccumulator(file, size_guidance={event_accumulator.SCALARS: 0,})
             ea.Reload()
             
             # print(ea.Tags())
-            trained_steps = len(ea.Scalars('Train/Loss'))
+            try:
+                trained_steps = len(ea.Scalars('Train/Loss'))
+            except KeyError:
+                trained_steps = len(ea.Scalars('Train/Loss_v2'))
 
             if trained_steps >= 10000:
                 color = bcolors.OKGREEN
+                delete_it = False
             else:
                 color = bcolors.WARNING
-
+                delete_it = True
 
             print(f"{color}{file}\t{trained_steps}{bcolors.ENDC}")
-        except KeyError:  # Doesn't have the 'Train/Loss' tag, then delete it.
-            parent_folder = pathlib.Path(file).parent
             
+            if delete_it:
+                raise KeyError
+        
+        except KeyError:  # Doesn't have the 'Train/Loss' or 'Train/Loss_v2' tags, then delete it.
+            parent_folder = pathlib.Path(file).parent
+
             # print(file)
             print(f"Deleting '{parent_folder}' ...")
 
